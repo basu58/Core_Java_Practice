61b03b11c624ab8e32d94919132267b84214d331
basu58 <basudevchhotray@gmail.com>
2021-06-22 15:50:16 +0530

    hackerrank

---
+++ Hackerrank/src/com/basu/hackerrank/Ex2.java
@@ -0,0 +1,11 @@
+package com.basu.hackerrank;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		String s = "Hello", s1 = "java";
+		System.out.println(s1.compareTo(s));
+		
+		
+		
+	}
+}
